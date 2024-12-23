4614d6c460848d5f1331316df9c6551332c8303a
Vivek <the9416legend@gmail.com>
2024-12-23 19:47:18 +0530

    [Vivek] modified : updated employeeWage.cpp with use-case-6

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -24,40 +24,50 @@ int main() {
 
     const int wagePerHour = 20 ;
     const int totalWorkingDays = 20 ; 
+    const int maxWorkingHours = 100 ;
 
     int totalWageMonth = 0 ;
+    int totalWorkingHrs = 0 ;
+    int daysWorked = 0 ; 
 
-    for(int i = 1 ; i <= totalWorkingDays ; i++){
-
-
-    // Check attendance
-    int attendance = markAttendance();
-
-    switch (attendance) {
-    case 0: // Employee is absent
-        cout << "Employee is absent." << endl;
-        break;
-
-    case 1: { // Employee is present
-        cout << "Employee is present." << endl;
-
-        // Calculate wage based on working hours
-        int wagePerHour = 20; // Wage per hour
-        int workingHours = getRandomWorkingHours();
-
-        switch (workingHours > 4) {
-        case 1: { // Full-time employee
-            int dailyWage = calculateDailyWage(wagePerHour, workingHours);
-            totalWageMonth += dailyWage ;
-            cout << "Employee worked full-time for " << workingHours << " hours." << endl;
-            cout << "Daily wage of Employee is: " << dailyWage << endl;
-            break;
+    for(int i = 1 ; i <= totalWorkingDays ; i++)
+    {
+        if(totalWorkingHrs >= maxWorkingHours) {
+            cout << "Maximum Working hours reached . " << endl ;
+            break;   
         }
+
+        // Check attendance
+        int attendance = markAttendance();
+
+        switch (attendance) {
+            case 0: // Employee is absent
+                cout << "Employee is absent." << endl;
+                break;
+
+            case 1: { // Employee is present
+                cout << "Employee is present." << endl;
+
+                // Calculate wage based on working hours
+                int workingHours = getRandomWorkingHours();
+
+                if(totalWorkingHrs + workingHours > maxWorkingHours){
+                    workingHours = maxWorkingHours - totalWorkingHrs ;
+                }
+                    totalWorkingHrs += workingHours ;
+                    daysWorked++;
+
+                switch (workingHours > 4) {
+                    case 1: { // Full-time employee
+                        int dailyWage = calculateDailyWage(wagePerHour, workingHours);
+                        totalWageMonth += dailyWage ;
+                        cout << "Employee worked full-time for : " << workingHours << " hours." << endl;
+                        break;
+                        }
         case 0: { // Part-time employee
             int partTimeWage = calculateDailyWage(wagePerHour, workingHours);
             totalWageMonth += partTimeWage ;
-            cout << "Part-time employee worked for " << workingHours << " hours." << endl;
-            cout << "Part-time wage of Employee is: " << partTimeWage << endl;
+            cout << "Employee worked part-time for :  " << workingHours << " hours." << endl;
             break;
         }
         }
@@ -70,6 +80,8 @@ int main() {
     }
 
     cout << "Total monthly wage : " << totalWageMonth << endl ;
+    cout << "Total Working hours : " << totalWorkingHrs << endl ;
+    cout << "Total Days Worked : " << daysWorked << endl ; 
 
     return 0;
 }
