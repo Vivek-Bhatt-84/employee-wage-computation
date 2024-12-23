11ddef0ea11ac90f3e05daa29f1317dda077c45a
Vivek <the9416legend@gmail.com>
2024-12-23 14:15:52 +0530

    [Vivek] modified : employee.cpp with use case 1

--- employeeWage.cpp
+++ employeeWage.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+// Function to calculate employee attenedence 
+void markAttendence(){
+    srand(time(0));
+    int random = rand() % 2 ;
+    
+    if(random == 0) 
+    {
+        cout << "Employee is absent . " << endl ;
+    }
+    else
+    {
+        cout << "Employee is present . " << endl ;
+    }
+}
+
 int main()
 {
     cout << "Welcome to Employee-Wage-computation" << endl ;
+
+    // attendence 
+    markAttendence();    
 
     return 0;
 }
